b60914b4ed6d20121b372c21d67790aeb63548b8
myyusuf <myyusuf1911@gmail.com>
2017-01-25 22:58:58 +0700

    With chart

---
+++ app/src/main/java/id/co/wika/pcddashboard/components/CustomMarkerView.java
@@ -0,0 +1,47 @@
+package id.co.wika.pcddashboard.components;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+
+import java.text.DecimalFormat;
+
+import id.co.wika.pcddashboard.R;
+
+/**
+ * Created by myyusuf on 11/14/16.
+ */
+public class CustomMarkerView extends MarkerView {
+
+    private TextView tvContent;
+    DecimalFormat decimalFormat = new DecimalFormat("#,###,###.00");
+
+    public CustomMarkerView (Context context, int layoutResource) {
+        super(context, layoutResource);
+        // this markerview only displays a textview
+        tvContent = (TextView) findViewById(R.id.tvContent);
+    }
+
+    // callbacks everytime the MarkerView is redrawn, can be used to update the
+    // content (user-interface)
+    @Override
+    public void refreshContent(Entry e, Highlight highlight) {
+        tvContent.setText(decimalFormat.format(e.getY())); // set the entry-value as the display text
+    }
+
+//    @Override
+//    public int getXOffset(float xpos) {
+//        // this will center the marker-view horizontally
+//        return -(getWidth() / 2);
+//    }
+//
+//    @Override
+//    public int getYOffset(float ypos) {
+//        // this will cause the marker-view to be above the selected value
+//        return -getHeight();
+//    }
+}
+
--- app/src/main/java/id/co/wika/pcddashboard/fragments/RiFragment.java
+++ app/src/main/java/id/co/wika/pcddashboard/fragments/RiFragment.java
@@ -1,14 +1,32 @@
 package id.co.wika.pcddashboard.fragments;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.formatter.DefaultAxisValueFormatter;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import id.co.wika.pcddashboard.DashboardConstant;
 import id.co.wika.pcddashboard.R;
+import id.co.wika.pcddashboard.components.CustomMarkerView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +47,9 @@ public class RiFragment extends Fragment {
     private String mParam2;
 
     private OnRiFragmentInteractionListener mListener;
+
+    private LineChart mChart;
+    private String[] MONTHS;
 
     public RiFragment() {
         // Required empty public constructor
@@ -65,7 +86,43 @@ public class RiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ri, container, false);
+
+        this.MONTHS = new String[14];
+        this.MONTHS[0] = "";
+        this.MONTHS[1] = DashboardConstant.MONTHS[0];
+        this.MONTHS[2] = DashboardConstant.MONTHS[1];
+        this.MONTHS[3] = DashboardConstant.MONTHS[2];
+        this.MONTHS[4] = DashboardConstant.MONTHS[3];
+        this.MONTHS[5] = DashboardConstant.MONTHS[4];
+        this.MONTHS[6] = DashboardConstant.MONTHS[5];
+        this.MONTHS[7] = DashboardConstant.MONTHS[6];
+        this.MONTHS[8] = DashboardConstant.MONTHS[7];
+        this.MONTHS[9] = DashboardConstant.MONTHS[8];
+        this.MONTHS[10] = DashboardConstant.MONTHS[9];
+        this.MONTHS[11] = DashboardConstant.MONTHS[10];
+        this.MONTHS[12] = DashboardConstant.MONTHS[11];
+        this.MONTHS[13] = "";
+
+
+        final View view = inflater.inflate(R.layout.fragment_ri, container, false);
+
+        mChart = (LineChart)view.findViewById(R.id.omzet_chart);
+
+        List<Entry> planDataEntries = new ArrayList<Entry>();
+        List<Entry> actualDataEntries = new ArrayList<Entry>();
+
+        planDataEntries.add(new Entry(1, new Float(1)));
+        planDataEntries.add(new Entry(2, new Float(4)));
+        planDataEntries.add(new Entry(3, new Float(2)));
+        planDataEntries.add(new Entry(4, new Float(7)));
+
+
+        actualDataEntries.add(new Entry(1, new Float(1)));
+        actualDataEntries.add(new Entry(2, new Float(3)));
+
+        drawChart(planDataEntries, actualDataEntries);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -105,5 +162,111 @@ public class RiFragment extends Fragment {
     public interface OnRiFragmentInteractionListener {
         // TODO: Update argument type and name
         void onRiFragmentInteraction(Uri uri);
+    }
+
+    private void drawChart(List<Entry> planDataEntries, List<Entry> actualDataEntries){
+
+        if(planDataEntries.size() > 11) {
+            int lastIndex = planDataEntries.size() - 1;
+            float lastValue = planDataEntries.get(lastIndex).getY();
+            planDataEntries.add(new Entry(13, lastValue));
+
+        }
+        planDataEntries.add(0, new Entry(0, 0));
+
+        if(actualDataEntries.size() > 11) {
+            int lastIndex = actualDataEntries.size() - 1;
+            float lastValue = actualDataEntries.get(lastIndex).getY();
+            actualDataEntries.add(new Entry(13, lastValue));
+        }
+        actualDataEntries.add(0, new Entry(0, 0));
+
+//        this.planDataEntries = planDataEntries;
+//        this.actualDataEntries = actualDataEntries;
+
+        mChart.setMinimumHeight(450);
+//        mChart.setDescription("");
+//        mChart.setNoDataTextDescription("No data.");
+
+
+        LineDataSet dataSet = new LineDataSet(planDataEntries, "Plan");
+        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        dataSet.setDrawCircles(false);
+        dataSet.setDrawFilled(true);
+        dataSet.setDrawValues(false);
+        dataSet.setColor(Color.parseColor("#8CEAFF"));
+        dataSet.setFillColor(Color.parseColor("#8CEAFF"));
+        dataSet.setFillAlpha(70);
+
+        LineDataSet dataSet2 = new LineDataSet(actualDataEntries, "Actual");
+        dataSet2.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        dataSet2.setDrawCircles(false);
+        dataSet2.setDrawFilled(true);
+        dataSet2.setDrawValues(false);
+        dataSet2.setColor(Color.parseColor("#F7E81C"));
+        dataSet2.setFillColor(Color.parseColor("#F7E81C"));
+        dataSet2.setFillAlpha(200);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        dataSets.add(dataSet2);
+        dataSets.add(dataSet);
+
+        LineData lineData = new LineData(dataSets);
+
+
+        DefaultAxisValueFormatter formatter = new DefaultAxisValueFormatter(0) {
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                int valueInInt = (int) value;
+                if(valueInInt >= 0 && valueInInt < RiFragment.this.MONTHS.length){
+                    return RiFragment.this.MONTHS[(int) value];
+                }else{
+                    return "";
+                }
+
+            }
+
+            @Override
+            public int getDecimalDigits() {  return 0; }
+        };
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setGranularity(1f);
+        xAxis.setValueFormatter(formatter);
+
+        mChart.getAxisLeft().setSpaceBottom(0);
+        mChart.setDrawBorders(false);
+        mChart.getAxisRight().setEnabled(false);
+        mChart.getXAxis().setGridColor(Color.GRAY);
+        mChart.getLegend().setEnabled(false);
+
+        mChart.getXAxis().setTextColor(Color.WHITE);
+        mChart.getXAxis().setPosition(XAxis.XAxisPosition.TOP_INSIDE);
+        mChart.getXAxis().setDrawAxisLine(false);
+
+        mChart.getAxisLeft().setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+        mChart.getAxisLeft().setEnabled(false);
+        mChart.setViewPortOffsets(0, 15, 0, 0);
+
+        mChart.setPinchZoom(true);
+
+        mChart.setData(lineData);
+        mChart.invalidate();
+
+        mChart.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                mChart.requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+
+        CustomMarkerView mv = new CustomMarkerView(getActivity().getBaseContext(), R.layout.custom_marker_view);
+        mChart.setMarkerView(mv);
+
+//        updateSelectedMonth(selectedMonth);
+
     }
 }
