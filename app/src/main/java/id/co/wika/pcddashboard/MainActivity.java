03415c0108b96bb0beb7ba8dcfa57ad601ae3de8
myyusuf <myyusuf1911@gmail.com>
2017-03-15 15:01:36 +0700

    Dashboard new design

--- app/src/main/java/id/co/wika/pcddashboard/MainActivity.java
+++ app/src/main/java/id/co/wika/pcddashboard/MainActivity.java
@@ -83,6 +83,8 @@ public class MainActivity extends AppCompatActivity implements
 
     private FragmentTabHost mTabHost;
 
+    private TabHost host;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -157,26 +159,26 @@ public class MainActivity extends AppCompatActivity implements
 //            tv.setTextColor(Color.parseColor("#ffffff"));
 //        }
 
-        TabHost host = (TabHost)findViewById(R.id.tabHost);
-        host.setup();
-
-        //Tab 1
-        TabHost.TabSpec spec = host.newTabSpec("Tab One");
-        spec.setContent(R.id.tab1);
-        spec.setIndicator("Tab One");
-        host.addTab(spec);
-
-        //Tab 2
-        spec = host.newTabSpec("Tab Two");
-        spec.setContent(R.id.tab2);
-        spec.setIndicator("Tab Two");
-        host.addTab(spec);
-
-        //Tab 3
-        spec = host.newTabSpec("Tab Three");
-        spec.setContent(R.id.tab3);
-        spec.setIndicator("Tab Three");
-        host.addTab(spec);
+//        host = (TabHost)findViewById(R.id.tabHost);
+//        host.setup();
+//
+//        //Tab 1
+//        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+//        spec.setContent(R.id.tab1);
+//        spec.setIndicator("Tab One");
+//        host.addTab(spec);
+//
+//        //Tab 2
+//        spec = host.newTabSpec("Tab Two");
+//        spec.setContent(R.id.tab2);
+//        spec.setIndicator("Tab Two");
+//        host.addTab(spec);
+//
+//        //Tab 3
+//        spec = host.newTabSpec("Tab Three");
+//        spec.setContent(R.id.tab3);
+//        spec.setIndicator("Tab Three");
+//        host.addTab(spec);
     }
 
     @Override
